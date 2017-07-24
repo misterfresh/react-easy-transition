89b693a186ef52730a5b7f30608d67e26f3bb98f
Giorgi Bagdavadze <notgiorgi@gmail.com>
2017-07-24 12:44:32 +0400

    lint and prettify

--- src/TransitionChild.js
+++ src/TransitionChild.js
@@ -6,36 +6,53 @@ export default class TransitionChild extends Component {
   componentWillAppear(callback) {
     this.componentFadeIn(callback)
   }
+
   componentWillEnter(callback) {
     this.componentFadeIn(callback)
   }
+
   componentFadeIn(callback) {
-    let page = this.page, finalStyle = this.props.finalStyle
+    let page = this.page,
+      finalStyle = this.props.finalStyle
     Object.assign(this.page.style, this.props.initialStyle)
-    Object.keys(this.props.initialStyle).forEach(property => window.getComputedStyle(this.page)[property])
+    Object.keys(this.props.initialStyle).forEach(
+      property => window.getComputedStyle(this.page)[property]
+    )
     this.page.style.transition = this.props.transition
-    setTimeout( () => Object.assign(page.style, finalStyle) )
+    setTimeout(() => Object.assign(page.style, finalStyle))
     let transitionsRemaining = this.props.transition.split(',').length
-    this.page.addEventListener("transitionend", (event) => {
-      transitionsRemaining--
-      if (transitionsRemaining) return
-      callback()
-    }, false)
+    this.page.addEventListener(
+      'transitionend',
+      () => {
+        transitionsRemaining--
+        if (transitionsRemaining) return
+        callback()
+      },
+      false
+    )
   }
+
   componentWillLeave(callback) {
     let leaveStyle = this.props.leaveStyle ? this.props.leaveStyle : this.props.initialStyle
     Object.assign(this.page.style, leaveStyle)
     let transitionsRemaining = this.props.transition.split(',').length
-    this.page.addEventListener("transitionend", (event) => {
-      transitionsRemaining--
-      if (transitionsRemaining) return
-      callback()
-      this.props.childDidLeave()
-    }, false)
+    this.page.addEventListener(
+      'transitionend',
+      () => {
+        transitionsRemaining--
+        if (transitionsRemaining) return
+        callback()
+        this.props.childDidLeave()
+      },
+      false
+    )
   }
+
   render() {
-    return <div ref={(ref) => this.page = ref} style={this.props.initialStyle}>
-      {this.props.children}
-    </div>
+    return (
+      <div ref={ref => (this.page = ref)} style={this.props.initialStyle}>
+        {this.props.children}
+      </div>
+    )
   }
 }
